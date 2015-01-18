f342ac6db7d7105cc9b3b61879ae662f1a9b02f6
Joseph Kain <joekain@gmail.com>
2015-01-18 13:23:43 -0800

    Mix.Task.Bmark
    Skeleton task.

---
+++ lib/mix/tasks/bmark.ex
@@ -0,0 +1,5 @@
+defmodule Mix.Tasks.Bmark do
+  use Mix.Task
+  
+  def run(args), do: true
+end
--- test/end_to_end/bmark_task_test.sh
+++ test/end_to_end/bmark_task_test.sh
@@ -3,5 +3,5 @@
 . end_to_end.sh "It has a Mix task named 'bmark'"
 
 cd ../../
-mix help bmark || fail
+mix help bmark > /dev/null || fail
 pass
