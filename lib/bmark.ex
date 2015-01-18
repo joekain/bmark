77e0a2331c6948ecabe29566ab1acc38277312c7
Joseph Kain <joekain@gmail.com>
2015-01-18 14:59:09 -0800

    Load bmark files

--- lib/bmark.ex
+++ lib/bmark.ex
@@ -1,2 +1,10 @@
 defmodule Bmark do
+  defmacro __using__(_) do
+    quote do
+      import unquote(__MODULE__), only: :macros
+    end
+  end
+
+  defmacro bmark(_name, [do: _body]) do
+  end
 end
--- lib/mix/tasks/bmark.ex
+++ lib/mix/tasks/bmark.ex
@@ -2,8 +2,21 @@ defmodule Mix.Tasks.Bmark do
   use Mix.Task
   
   def run(_args) do
+    setup_project
+    setup_bmark_files
+
     File.open("results/example.runner.results", [:write], fn(file) ->
       IO.puts(file, "Some results")
     end)
+  end
+
+  defp setup_project do
+    Mix.Project.get!
+    Mix.Task.run("compile", [])
+    Mix.Task.run("loadpaths", [])
+  end
+
+  defp setup_bmark_files do
+    Path.wildcard("bmark/**/*_bmark.ex") |> Kernel.ParallelRequire.files()
   end
 end
