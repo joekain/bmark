e6c11189991f011fe629e742bc2127b303eef1a1
Joseph Kain <joekain@gmail.com>
2015-01-18 22:18:57 -0800

    Implement Bmark.Cmp
    * Load the results
    * Compute the stats
    * Report the result
    * Implement the critical t distribution values lookup
    
    Passes bmark_cmp_different_results_test

--- lib/mix/tasks/bmark_cmp.ex
+++ lib/mix/tasks/bmark_cmp.ex
@@ -1,7 +1,99 @@
 defmodule Mix.Tasks.Bmark.Cmp do
   use Mix.Task
   
-  def run(_args) do
-    IO.puts "p < 1"
+  defmodule Stats do
+    defstruct count: 0, mean: 0, stdev: 0
+  end
+
+  def run(args) do
+    args
+    |> parse_args
+    |> compare
+    |> report_results
+  end
+
+  defp parse_args(args) do
+    case OptionParser.parse(args, strict: []) do
+      {[], [name1, name2], []} -> [name1, name2]
+      {_, _, _}                -> usage
+    end
+  end
+
+  defp usage do
+    Kernel.exit("Usage: mix lifebench.cmp <results file 1> <results file 2>")
+  end
+
+  defp compare(array_of_files) do
+    array_of_files
+    |> Enum.map(&load_results(&1))
+    |> Enum.map(&compute_stats(&1))
+    |> compute_t_value
+  end
+
+  defp load_results(results_file_name) do
+    File.stream!(results_file_name)
+    |> Enum.to_list
+    |> Enum.map(&String.strip(&1))
+    |> Enum.map(&String.to_integer(&1))
+  end
+
+  defp compute_stats(results) do
+    %Stats
+    {
+      count: Enum.count(results),
+      mean:  sample_mean(results),
+      stdev: corrected_sample_stdev(results)
+    }
+  end
+
+  defp sample_mean(samples) do
+    Enum.sum(samples) / Enum.count(samples)
+  end
+
+  defp corrected_sample_stdev(samples) do
+    mean = sample_mean(samples)
+    (Enum.map(samples, fn(x) -> (mean - x) * (mean - x) end) |> Enum.sum) / (Enum.count(samples) - 1)
+    |> :math.sqrt
+  end
+
+  defp compute_t_value(stats) do
+    a = compute_a(stats)
+    b = compute_b(stats)
+    t = compute_t(a, b, stats)
+    {stats, t}
+  end
+
+  defp compute_a([%Stats{count: n1}, %Stats{count: n2}]) do
+    (n1 + n2) / (n1 * n2)
+  end
+
+  defp compute_b([%Stats{count: n1, stdev: s1}, %Stats{count: n2, stdev: s2}]) do
+    ( ((n1 - 1) * s1 * s1) + ((n2 - 1) * s2 * s2) ) / (n1 + n2 - 2)
+  end
+
+  defp compute_t(a, b, [%Stats{mean: u1}, %Stats{mean: u2}]) do
+    abs(u1 - u2) / :math.sqrt(a * b)
+  end
+
+  defp report_results({[%Stats{mean: u1}, %Stats{mean: u2}] = stats, t}) do
+    IO.puts "#{u1} -> #{u2} (#{percent_increase(u1, u2)}) with p < #{t_dist(t, df(stats))}"
+    IO.puts "t = #{t}, #{df(stats)} degrees of freedom"
+  end
+
+  defp df([%Stats{count: n1}, %Stats{count: n2}]) do
+    n1 + n2 - 2
+  end
+
+  def percent_increase(u1, u2) do
+    percent = 100 * (u2 - u1) / u1
+    percent_s = Float.to_string(percent, [decimals: 2])
+    cond do
+      percent < 0 ->  "#{percent_s}%"
+      true        -> "+#{percent_s}%"
+    end
+  end
+
+  defp t_dist(t, df) do
+    Bmark.Distribution.t(t, df)
   end
 end
--- test/end_to_end/end_to_end_config.sh
+++ test/end_to_end/end_to_end_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export END_TO_END_TEST_DIR="./"
-export END_TO_END_DATA_DIR="./test_data"
-export END_TO_END_GOLD_DIR="./gold"
-export END_TO_END_OUTPUT_DIR="./_out"
+export END_TO_END_TEST_DIR=$(pwd)
+export END_TO_END_DATA_DIR="$(pwd)/test_data"
+export END_TO_END_GOLD_DIR="$(pwd)/gold"
+export END_TO_END_OUTPUT_DIR="$(pwd)/_out"
 
