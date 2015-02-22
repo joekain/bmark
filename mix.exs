46558f48ef760d5de2e3305beb96ab9dd45d9c11
Joseph Kain <joekain@gmail.com>
2015-02-22 15:16:59 -0800

    Add Hex package data

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule Bmark.Mixfile do
      version: "1.0.0",
      elixir: "~> 1.0.0",
      deps: deps,
-     description: description]
+     description: description,
+     package: package]
   end
 
   def application do
@@ -16,10 +17,20 @@ defmodule Bmark.Mixfile do
   defp deps do
     [{:inch_ex, only: :docs}]
   end
-  
+
   defp description do
     """
     A benchmarking tool for Elixr with a focus on comparing results with confidence.
     """
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE", "CONTRIBUTING.md"],
+     contributors: ["Joseph Kain"],
+     licenses: ["MIT"],
+     links: %{
+       "github" => "https://github.com/joekain/bmark",
+       "Blog posts" => "http://learningelixir.joekain.com/bmark-posts/"
+     }]
   end
 end
